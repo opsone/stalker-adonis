fc604604795b6be48f2242cc4f795b5d3a12616f
jeremy <jeremy.chaufourier@opsone.net>
2025-12-23 14:06:15 +0100

    feat: add default config for redis

--- src/define_config.ts
+++ src/define_config.ts
@@ -4,6 +4,8 @@ export function defineConfig<T extends StalkerConfig>(config: T): T {
   return {
     npmBin: 'npm',
     pnpmBin: 'pnpm',
+    redisBin: 'redis-cli',
+    redisHost: 'localhost',
     ...config,
   }
 }
